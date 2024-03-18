726a7882bd5f93e88491bfabcacf6d5c9f68e483
Matheus Henrique Mendes <matthew.coder@outlook.com>
2024-03-18 16:48:51 -0300

    add try catch

--- DS_Agenda_05/src/App.java
+++ DS_Agenda_05/src/App.java
@@ -2,29 +2,41 @@ package DS_Agenda_05.src;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 import DS_Agenda_05.src.Class.Console;
 import DS_Agenda_05.src.Class.Shopping;
 
 public class App {
-   
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
         Console console = new Console();
-        
+
         console.header();
-        
-        console.print("Enter a value: ");
-        float value = scan.nextFloat();
-
-        Shopping shop = new Shopping(value);
-
-        shop.status();
 
 
+        String aux = "none";
+        float value = 0f;
+
+        try {
+
+            aux = JOptionPane.showInputDialog(null, "Enter a value: ");
+            value = Float.parseFloat(aux);
+
+            Shopping shop = new Shopping(value);
+            shop.status();
+
+        } catch (NumberFormatException e) {
+
+            String erro = String.format(" Somente numero! [%s]", aux);
+            System.out.println(erro);       
+
+        }
 
         console.space();
         scan.close();
     }
-    
+
 }
