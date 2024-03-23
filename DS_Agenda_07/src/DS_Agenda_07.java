0c3e11bc7894c88dd22eba845af36f8e58e58095
Matheus Henrique Mendes <matthew.coder@outlook.com>
2024-03-23 11:30:44 -0300

    add: JOptionPane - complete

--- DS_Agenda_07/src/DS_Agenda_07.java
+++ DS_Agenda_07/src/DS_Agenda_07.java
@@ -17,7 +17,8 @@ public class DS_Agenda_07 {
         int size_Ruim = 0;
         double full_Age = 0.0;
 
-        JOptionPane.showMessageDialog(null, "DS_Agenda_07");
+        String mensage = String.format("DS_Agenda_07 \n Matheus Henrique da Silva Mendes");
+        JOptionPane.showMessageDialog(null, mensage);
 
         for (int i = 0; i < ATENDIMENTO.USERS.length; i++) {
             String string_To_Integer = "none";
@@ -33,9 +34,8 @@ public class DS_Agenda_07 {
             ATENDIMENTO.USERS[i] = name;
             full_Age = full_Age + age;
 
-            System.out.println("Selecione o nivel de atendimento abaixo: ");
+            mensage="Selecione o nivel de atendimento abaixo: \n";
 
-            String mensage="";
             for (int a = 0; a < ATENDIMENTO.ATENDIMENTOS.length; a++) {
                 mensage = mensage + String.format("[%d] %s\n", a, ATENDIMENTO.ATENDIMENTOS[a]);
                 
@@ -56,7 +56,7 @@ public class DS_Agenda_07 {
                         size_Ruim++;
 
                     } else {
-                        System.out.println("digito invalido");
+                        JOptionPane.showConfirmDialog(null,"digito invalido","ERRO", JOptionPane.ERROR_MESSAGE);
                     }
                 }
 
@@ -66,18 +66,21 @@ public class DS_Agenda_07 {
 
         double media = full_Age / ATENDIMENTO.PARTICIPANTES;
 
-        System.out.println("----------------------------------------------");
-        System.out.println("Resultado da pesquisa:");
-        System.out.println("Exelente: " + size_Execelente);
-        System.out.println("Bom: " + size_Bom);
-        System.out.println("Ruim: " + size_Ruim);
-        System.out.printf("Media de idades: %f", media);
+        mensage = String.format("-----------------------------------------------------\n Resultado da pesquisa:\n Exelente: %d \n Bom: %d \n Ruim %d \n ----------------------------------------------------- \n Media das idades: %f", 
+        size_Execelente, size_Bom, size_Ruim, media);
 
+        JOptionPane.showMessageDialog(null, mensage);
 
-        System.out.println("Participantes:");
+        mensage = String.format("Participantes:\n");
+
         for(int i = 0; i < ATENDIMENTO.USERS.length; i++){
-            System.out.printf("[%d] %s\n", i, ATENDIMENTO.USERS[i]);
+            mensage = mensage + String.format("[%d] %s\n", i, ATENDIMENTO.USERS[i]);
         }
+
+        JOptionPane.showMessageDialog(null, mensage);
+
+
+        
         scan.close();
     }
 }
