b8b5f6ccc7335e7f7d391850d29eb8c0345259eb
w10 <matheus.h.da.s.mendes13@gmail.com>
2024-05-26 12:48:48 -0300

    add: new sections

--- DS_Agenda_15/assets/javascript/script.js
+++ DS_Agenda_15/assets/javascript/script.js
@@ -35,10 +35,13 @@ function menuOff() {
     const menu_button = document.getElementById("menu--logo");
     let atribute = menu_button.getAttribute("class");
 
-    if (atribute === "menu--logo--active") {
+    const element_main = document.getElementById("main");
+
+    if (atribute === "menu--logo") {
 
         document.getElementById("sidebar").style.width = "0px";
-        menu_button.setAttribute("class", "menu--logo");
+        menu_button.setAttribute("class", "menu--logo--active");
 
+        element_main.style.marginLeft = "0px";
     }
 }
