b6159ef2ba94b3d8ce7f3195ec16cfe2c1fb2697
w10 <matheus.h.da.s.mendes13@gmail.com>
2024-05-30 17:20:55 -0300

    start DS_Agenda_16

---
+++ DS_Agenda_16/script.js
@@ -0,0 +1,71 @@
+
+function buttonActive() {
+    const menu_button = document.getElementById("menu--logo");
+    let atribute = menu_button.getAttribute("class");
+
+    if (atribute === "menu--logo--active") {
+        openMenu();
+    }
+    else {
+        closeMenu();
+    }
+}
+
+function openMenu() {
+    const menu_button = document.getElementById("menu--logo");
+    let atribute = menu_button.getAttribute("class");
+
+    document.getElementById("sidebar").style.width = "240px";
+    menu_button.setAttribute("class", "menu--logo");
+
+    document.querySelector("main").style.marginLeft = "240px";
+}
+
+function closeMenu() {
+    const menu_button = document.getElementById("menu--logo");
+    let atribute = menu_button.getAttribute("class");
+
+    document.getElementById("sidebar").style.width = "0px";
+    menu_button.setAttribute("class", "menu--logo--active")
+
+    document.querySelector("main").style.marginLeft = "0px";
+}
+
+function menuOff() {
+    const menu_button = document.getElementById("menu--logo");
+    let atribute = menu_button.getAttribute("class");
+
+    const element_main = document.getElementById("main");
+
+    if (atribute === "menu--logo") {
+
+        document.getElementById("sidebar").style.width = "0px";
+        menu_button.setAttribute("class", "menu--logo--active");
+
+        element_main.style.marginLeft = "0px";
+    }
+}
+
+function navBar(idTag){
+
+    document.getElementById("home--content").style.display = "none";
+    document.getElementById("about--content").style.display = "none";
+    document.getElementById("grade--content").style.display = "none";
+    document.getElementById("docentes--content").style.display = "none";
+    document.getElementById("mercado--content").style.display = "none";
+    document.getElementById("tecnologias--content").style.display = "none";
+    document.getElementById("contato--content").style.display = "none";
+    
+
+   document.getElementById(idTag).style.display = "flex";
+}
+
+function defultPage(){
+    document.getElementById("home--content").style.display = "flex";
+}
+defultPage();
+
+
+function slider(){
+     
+}
