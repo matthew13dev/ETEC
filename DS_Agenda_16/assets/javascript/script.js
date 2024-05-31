2a1803fde176646c8a3b31ffd7b59df3346dbe8b
w10 <matheus.h.da.s.mendes13@gmail.com>
2024-05-30 21:47:27 -0300

    change ds16

--- DS_Agenda_16/assets/javascript/script.js
+++ DS_Agenda_16/assets/javascript/script.js
@@ -46,7 +46,7 @@ function menuOff() {
     }
 }
 
-function navBar(idTag){
+function navBar(idTag) {
 
     document.getElementById("home--content").style.display = "none";
     document.getElementById("about--content").style.display = "none";
@@ -55,17 +55,45 @@ function navBar(idTag){
     document.getElementById("mercado--content").style.display = "none";
     document.getElementById("tecnologias--content").style.display = "none";
     document.getElementById("contato--content").style.display = "none";
-    
 
-   document.getElementById(idTag).style.display = "flex";
+
+    document.getElementById(idTag).style.display = "flex";
 }
 
-function defultPage(){
+function defultPage() {
     document.getElementById("home--content").style.display = "flex";
 }
 defultPage();
 
 
-function slider(){
-     
+const sliderSetings = {
+    elements: {
+        widthElement: document.querySelector(".slider--width"),
+        itemsElements: document.querySelectorAll(".slider--item"),
+    },
+
+    width: function () {
+        this.widthElement.style.width = `calc(100% * ${this.elements.itemsElements.length}`
+    },
+
+    behindEvent: function () {
+        document.querySelector(".behind").addEventListener("click", ()=>{
+            console.log("behind!")
+        })
+    },
+    afterEvent: function () {
+        document.querySelector(".after").addEventListener("click", ()=>{
+            console.log("after!")
+        })
+        
+    }
 }
+
+
+
+sliderSetings.width();
+sliderSetings.behindEvent();
+sliderSetings.afterEvent();
+
+
+
