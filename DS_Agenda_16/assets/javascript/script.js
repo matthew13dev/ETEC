46daf19f8bcb9a93c8f228de921471e2aff608f8
w10 <matheus.h.da.s.mendes13@gmail.com>
2024-05-30 22:20:58 -0300

    change>sidebar

--- DS_Agenda_16/assets/javascript/script.js
+++ DS_Agenda_16/assets/javascript/script.js
@@ -1,51 +1,3 @@
-
-function buttonActive() {
-    const menu_button = document.getElementById("menu--logo");
-    let atribute = menu_button.getAttribute("class");
-
-    if (atribute === "menu--logo--active") {
-        openMenu();
-    }
-    else {
-        closeMenu();
-    }
-}
-
-function openMenu() {
-    const menu_button = document.getElementById("menu--logo");
-    let atribute = menu_button.getAttribute("class");
-
-    document.getElementById("sidebar").style.width = "240px";
-    menu_button.setAttribute("class", "menu--logo");
-
-    document.querySelector("main").style.marginLeft = "240px";
-}
-
-function closeMenu() {
-    const menu_button = document.getElementById("menu--logo");
-    let atribute = menu_button.getAttribute("class");
-
-    document.getElementById("sidebar").style.width = "0px";
-    menu_button.setAttribute("class", "menu--logo--active")
-
-    document.querySelector("main").style.marginLeft = "0px";
-}
-
-function menuOff() {
-    const menu_button = document.getElementById("menu--logo");
-    let atribute = menu_button.getAttribute("class");
-
-    const element_main = document.getElementById("main");
-
-    if (atribute === "menu--logo") {
-
-        document.getElementById("sidebar").style.width = "0px";
-        menu_button.setAttribute("class", "menu--logo--active");
-
-        element_main.style.marginLeft = "0px";
-    }
-}
-
 function navBar(idTag) {
 
     document.getElementById("home--content").style.display = "none";
@@ -78,12 +30,12 @@ const sliderSetings = {
 
     behindEvent: function () {
         document.querySelector(".behind").addEventListener("click", ()=>{
-            console.log("behind!")
+            alert("behind!")
         })
     },
     afterEvent: function () {
         document.querySelector(".after").addEventListener("click", ()=>{
-            console.log("after!")
+            alert("afeter!")
         })
         
     }
